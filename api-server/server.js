1075a0e08425251df96e09a50f41b6c28faf19a4
Sergey Novikov <synopticum@gmail.com>
2018-05-13 11:23:11 +0300

    Added cors module

--- api-server/server.js
+++ api-server/server.js
@@ -2,15 +2,15 @@ const db  = require('./db');
 const fastify = require('fastify')();
 const ObjectModel = require('./db/object.model');
 
+fastify.use(require('cors')());
+
 fastify.get('/api/objects/coordinates/paths', async (request, reply) => {
-    reply.header('Access-Control-Allow-Origin', '*');
     reply.type('application/json').code(200);
     const paths = await ObjectModel.find({ type: 'path' });
     return paths;
 });
 
 fastify.get('/api/objects/coordinates/circles', async (request, reply) => {
-    reply.header('Access-Control-Allow-Origin', '*');
     reply.type('application/json').code(200);
     const circles = await ObjectModel.find({ type: 'circle' }).select({ '_id': 0, 'type': 0});
     return circles;
