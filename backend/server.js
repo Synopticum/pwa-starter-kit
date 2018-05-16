4555eeac94b41fac734716f3a5c80f2b9a4546b9
Sergey Novikov <synopticum@gmail.com>
2018-05-16 21:06:50 +0300

    Eliminate unnecessary request

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,16 @@ function build (opts) {
 
     async function verifyVkAuth(request, reply, done) {
         let accessToken = request.headers['vk-access-token'];
-        let response = await fetch(`https://api.vk.com/method/users.get?access_token=${accessToken}&v=5.74`);
-        let json = await response.json();
 
-        if (!json.error) {
-            done();
-        } else {
-            done(new Error('Unauthorized'));
+        if (accessToken) {
+            let response = await fetch(`https://api.vk.com/method/users.get?access_token=${accessToken}&v=5.74`);
+            let json = await response.json();
+
+            if (!json.error) {
+                done();
+            } else {
+                done(new Error('Unauthorized'));
+            }
         }
     }
 
