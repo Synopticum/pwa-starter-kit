5f689dbe81e2ba29aa336bab881d90733a85731b
Sergey Novikov <synopticum@gmail.com>
2018-06-01 01:31:44 +0300

    Close object info

--- src/components/app-level/u-object-info/u-object-info.js
+++ src/components/app-level/u-object-info/u-object-info.js
@@ -1,5 +1,14 @@
 import { html, LitElement } from '@polymer/lit-element';
 import { SharedStyles } from '../../shared-styles.js';
+import { connect } from 'pwa-helpers/connect-mixin';
+
+import { store } from '../../../store';
+import { hideObjectInfo } from '../../../actions/map.js';
+
+import map from '../../../reducers/map.js';
+store.addReducers({
+  map
+});
 
 class UObjectInfo extends LitElement {
   _render(props) {
@@ -16,13 +25,29 @@ class UObjectInfo extends LitElement {
             background-color: #ffffff;
             border: 1px solid green;
             z-index: 200;
+            pointer-events: all;
+        }
+        
+        .close {
+            cursor: pointer;
+            position: absolute;
+            right: -15px;
+            top: -15px;
+            width: 30px;
+            height: 30px;
+            background-color: #ff0000;
         }
       </style>
       
       <div class="object">
+        <div class="close" on-click="${UObjectInfo.close}"></div>
         <slot></slot>
       </div> 
     `
+  }
+
+  static close() {
+    store.dispatch(hideObjectInfo());
   }
 }
 
