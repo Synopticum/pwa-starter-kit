cbbfa5b2a7bbc98ab232ec95a93c35bb93d36e57
Sergey Novikov <synopticum@gmail.com>
2019-05-18 16:32:00 +0300

    Change prod env

--- environments/environments.js
+++ environments/environments.js
@@ -4,13 +4,13 @@ export const ENV = {
         api: 'http://localhost:3000'
     },
 
-    // prod: {
-    //   static: 'https://www.u-r-u-s-s-u.com',
-    //   api: 'https://www.u-r-u-s-s-u.com:12345'
-    // },
-
     prod: {
-        static: 'http://localhost:5000',
-        api: 'http://localhost:3000'
-    }
+      static: 'https://www.u-r-u-s-s-u.com',
+      api: 'https://www.u-r-u-s-s-u.com:12345'
+    },
+
+    // prod: {
+    //     static: 'http://localhost:5000',
+    //     api: 'http://localhost:3000'
+    // }
 };
