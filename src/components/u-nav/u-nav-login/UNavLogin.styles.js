7e92804d8ff1cdced19c9eb29b6496db23a280a0
Sergey Novikov <synopticum@gmail.com>
2020-08-20 18:14:46 +0300

    Updated nav login button styles

--- src/components/u-nav/u-nav-login/UNavLogin.styles.js
+++ src/components/u-nav/u-nav-login/UNavLogin.styles.js
@@ -42,23 +42,97 @@ export default css`
 }
         
 .login {
-    display: flex;
+    display: block;
+    position: relative;
     width: 44px;
     height: 44px;
-    justify-content: center;
-    align-items: center;
-    color: #ffffff;
-    z-index: 100;
-    opacity: .5;
+    border-radius: 10px;
+    cursor: pointer;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    background-size: 100%;
+    border: 2px solid #000;
+    box-shadow: inset 7px 7px 7px rgba(255,255,255,.1);
+    transition: box-shadow .2s, border-radius .2s;
+    margin-bottom: 7px;
+    background-image: url('/static/images/u-nav/u-nav-login/vk.svg');
+    background-size: 30px;
+}
+
+.login::after {
+    position: absolute;
+    right: -10px;
+    top: -2px;
+    width: 10px;
+    height: 48px;
+    background: #666;
+    opacity: 0;
     transition: opacity .3s;
 }
 
 .login:hover {
     opacity: 1;
+    filter: saturate(1);
 }
 
-.login svg {
-    width: 34px;
-    height: 34px;
+.login:hover::after {
+    opacity: 0;
+    top: 0;
+    height: 44px;
+}
+
+.login:active {
+    box-shadow: inset 3px 3px 3px rgba(0,0,0,.5);
+}
+
+.login[active] {
+    opacity: 1;
+}
+
+.login[disabled] {
+    pointer-events: none;
+    cursor: default;
+    filter: saturate(0);
+    opacity: .5;
+}
+
+.login .title {
+    --color: #000;
+    opacity: 0;
+    position: absolute;
+    left: calc(100% + 18px);
+    top: 4px;
+    display: flex;
+    height: 36px;
+    padding: 0 10px;
+    align-items: center;
+    background: var(--color);
+    border-radius: 5px;
+    font-size: 12px;
+    color: #fff;
+    text-transform: uppercase;
+    transition: opacity .3s;
+    white-space: nowrap;
+}
+
+.login:hover .title {
+    opacity: 1;
+}
+
+.title::before {
+    content:'';
+    display: block;
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: -8px;
+    top: calc(50% - 8px);
+    border-top: 8px solid transparent;
+    border-bottom: 8px solid transparent;
+    border-right: 8px solid var(--color);
+}
+
+.login[active] .title {
+    opacity: 0;
 }
 `;
