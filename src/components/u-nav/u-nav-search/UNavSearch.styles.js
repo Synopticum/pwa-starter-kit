f6e0353e5d9779554787ef9bddf408ee98d34f88
Sergey Novikov <synopticum@gmail.com>
2020-08-22 11:48:02 +0300

    Add arrow to search bar

--- src/components/u-nav/u-nav-search/UNavSearch.styles.js
+++ src/components/u-nav/u-nav-search/UNavSearch.styles.js
@@ -30,6 +30,18 @@ export default css`
     display: flex;
 }
 
+.controls::before {
+    content: '';
+    position: absolute;
+    left: 5px;
+    top: 43px;
+    width: 0; 
+    height: 0; 
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent; 
+    border-right: 10px solid #fff; 
+}
+
 .controls__textinput {
     display: block;
     width: 100%;
